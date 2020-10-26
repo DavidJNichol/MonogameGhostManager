769b1072003a1668396401e31aca36985c40a84c
David Nichol <david.nichol@loop.colum.edu>
2020-10-26 14:53:53 -0500

    Adds latest version.

---
+++ MonogameGhostManager/Ghost.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGameLibrary.Sprite;
+using MonoGameLibrary.Util;
+
+namespace MonogameGhostManager
+{
+    public enum GhostState { Chasing, Evading, Roving, Dead }
+
+    public class Ghost : DrawableSprite
+    {
+        GhostState state;
+
+        protected PacMan.MonogamePacMan pacMan;
+
+        public Texture2D ghostTexture;
+
+
+        public GhostState State
+        {
+            get { return state; }
+            set
+            {
+                if (state != value)
+                {
+                    state = value;
+                }
+            }
+        }
+
+        public Ghost(Game game, GhostManager m, PacMan.MonogamePacMan pacMan)
+            : base(game)
+        {
+            this.state = GhostState.Roving;
+        }
+
+
+            public void Evade()
+        {
+            this.state = GhostState.Evading;
+        }
+    }
+}
