e65da3b95b2ce4c4c0f8dcb8da751692e53a01fb
David Nichol <david.nichol@loop.colum.edu>
2020-11-02 23:31:36 -0600

    Reuploads missing projects

---
+++ ShotManager/GhostManager.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MonogameGhost.Ghost;
+
+namespace ShotManager
+{
+    class GhostManager : Microsoft.Xna.Framework.GameComponent
+    {
+        public List<Ghost> ghostList;
+        public GhostManager(Game game) : base(game)
+        {
+            ghostList = new List<Ghost>();
+        }
+
+
+
+    }
+}
